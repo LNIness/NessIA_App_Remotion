28c5407196fd7191128682a31945085b051b0bf2
Hélène Gouguenheim <h.gouguenheim@eness.fr>
2026-03-05 11:36:50 +0100

    Test transition slide ok

--- src/components/transitions/slide.ts
+++ src/components/transitions/slide.ts
@@ -1,6 +1,7 @@
 import { linearTiming } from '@remotion/transitions';
 import { slide } from '@remotion/transitions/slide';
 
+/**swipe vers la gauche */
 export const swipeLeft = {
   presentation: slide({ direction: 'from-right' }),
   timing: linearTiming({ durationInFrames: 15 }),
